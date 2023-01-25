2fee9d90aca0b76ad82091d895bf624b6b1e5471
Evgenii <evgeniikapchik@gmail.com>
2023-01-25 22:59:37 +0300

    work with DI

--- app/src/main/java/com/example/notecleanapp/data/local/database/NoteDatabase.kt
+++ app/src/main/java/com/example/notecleanapp/data/local/database/NoteDatabase.kt
@@ -1,10 +1,11 @@
 package com.example.notecleanapp.data.local.database
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.notecleanapp.data.local.dao.NoteDao
 import com.example.notecleanapp.domain.model.Note
 
 @Database(entities = [Note::class], version = 1)
-abstract class AppDatabase {
+abstract class NoteDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
 }
