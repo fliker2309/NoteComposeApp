43d3222572497cca450b7bc69b8c124645e8e22d
Evgenii <evgeniikapchik@gmail.com>
2023-01-25 22:29:59 +0300

    add repository and repositoryImpl

--- app/src/main/java/com/example/notecleanapp/domain/repository/NoteRepository.kt
+++ app/src/main/java/com/example/notecleanapp/domain/repository/NoteRepository.kt
@@ -1,10 +1,11 @@
 package com.example.notecleanapp.domain.repository
 
-import androidx.room.Dao
 import com.example.notecleanapp.domain.model.Note
 import kotlinx.coroutines.flow.Flow
 
-
 interface NoteRepository {
-
+    fun getAllNotes(): Flow<List<Note>>
+    suspend fun getNoteById(id: Long): Note?
+    suspend fun addNote(note: Note)
+    suspend fun deleteNote(note: Note)
 }
