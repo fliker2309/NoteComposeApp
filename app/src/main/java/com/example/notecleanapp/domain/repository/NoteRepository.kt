9ee1dc820045ed4bf7ad4b486de5d6583aa00dee
Evgenii <evgeniikapchik@gmail.com>
2023-01-25 22:13:35 +0300

    Data layer

---
+++ app/src/main/java/com/example/notecleanapp/domain/repository/NoteRepository.kt
@@ -0,0 +1,10 @@
+package com.example.notecleanapp.domain.repository
+
+import androidx.room.Dao
+import com.example.notecleanapp.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+
+interface NoteRepository {
+
+}
